fea88e9fbb201a60219ebf3c88c241f933b67336
AlexKot81 <99488632+AlexKot81@users.noreply.github.com>
2022-05-11 14:43:24 +0300

    Add files via upload

--- script/menu_mini.js
+++ script/menu_mini.js
@@ -18,14 +18,18 @@ minMenu.addEventListener('click', ()=>{
 	window_menu.classList.add('window_menu');
 	window_menu.style.top = getPosition+'px'
 	htmlElem.style.overflow = 'hidden';
-	menu_mini_text.style.right = '0'
+	//menu_mini_text.style.right = '0'
+	setTimeout(render_point_nav, 2 * 100, '0');
 });
 
 window_menu.addEventListener('click', ()=>{
+	
 	window_menu.classList.remove('window_menu');
 	window_menu.classList.add('noaktiv')
 	htmlElem.style.overflow = 'unset';
 	//menu_mini_text.style.right = '-260px'
+	render_point_nav('-260');
+	
 })
 
 const text_menu = itemMenu.map(values => {
@@ -37,4 +41,8 @@ const text_menu = itemMenu.map(values => {
 })
 
 menu_mini_text.append(...text_menu);
+
+const render_point_nav = (rightPoin)=>{
+	menu_mini_text.style.right = rightPoin + 'px';
+};
 
